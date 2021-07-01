fbdb27d9394ad9a1be34391bf763829abaf4f258
faaiz <faaizajaz@gmail.com>
2021-07-01 16:06:57 +0500

    Routing for api endpoints for Project model

--- base_project/urls.py
+++ base_project/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
+from rest_framework.urlpatterns import format_suffix_patterns
+
 
 urlpatterns = [
-    path('project/', views.project_list),
-    path('project/<int:pk>/', views.project_detail),
+    path('all/', views.ProjectList.as_view()),
+    path('key/<int:pk>/', views.ProjectDetail.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
