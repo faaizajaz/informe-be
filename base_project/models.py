39d12afb8e1cd8767f142a3d3c205bf939730208
faaiz <faaizajaz@gmail.com>
2021-07-01 16:06:35 +0500

    Added name field to project model

--- base_project/models.py
+++ base_project/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Project(models.Model):
+    name = models.CharField(verbose_name="Project name", max_length=500)
     short_description = models.CharField(
         verbose_name="Short description of project", max_length=1000
     )
