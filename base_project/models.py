96f5b7b3fede21a5180ddccea2f28cd6db66291d
faaiz <faaizajaz@gmail.com>
2021-07-05 17:15:20 +0500

    Field edits

--- base_project/models.py
+++ base_project/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from impact.models import Impact
 
 
 class Project(models.Model):
@@ -7,3 +8,9 @@ class Project(models.Model):
         verbose_name="Short description of project", max_length=1000
     )
     long_description = models.TextField(verbose_name="Long description of project")
+
+    # TODO: null should not be allowed in production
+    impact = models.OneToOneField(Impact, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.name
--- outcome/models.py
+++ outcome/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from intoutcome.models import IntOutcome
+from output.models import Output
 
 
 class Outcome(models.Model):
@@ -6,3 +8,11 @@ class Outcome(models.Model):
         verbose_name="Short description of outcome", max_length=1000
     )
     long_description = models.TextField(verbose_name="Long description of outcome")
+
+    # Can link directly to output, or via intermediate outcome
+    # TODO: Add something that checks a flag in project for whether intoutcome
+    intoutcome = models.ManyToManyField(IntOutcome, null=True, blank=True)
+    output = models.ManyToManyField(Output, null=True, blank=True)
+
+    def __str__(self):
+        return self.short_description
