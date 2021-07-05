24077a2df97d05e340648ce9c87a8972d68bcc54
faaiz <faaizajaz@gmail.com>
2021-07-05 17:15:00 +0500

    Trying nexted serializers

--- base_project/serializers.py
+++ base_project/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
 from .models import Project
+from impact.serializers import ImpactSerializer
+import json
 
 
 class ProjectSerializer(serializers.ModelSerializer):
+    impact = ImpactSerializer(read_only=False)
+
     class Meta:
         model = Project
-        fields = ['id', 'short_description', 'long_description']
+        fields = ['id', 'name', 'impact', 'short_description', 'long_description']
+        # depth = 6
