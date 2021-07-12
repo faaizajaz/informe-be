1586220d7f12046df3b23c626e22f13668a10535
faaiz <faaizajaz@gmail.com>
2021-07-12 19:14:15 +0500

    Added item serializers

--- serializers/flat_serializers.py
+++ serializers/flat_serializers.py
@@ -1,27 +1,60 @@
+from drf_writable_nested.serializers import WritableNestedModelSerializer
 from rest_framework import serializers
 from base_project.models import Item, Project
 
+# TODO: There will be 2 item serializers. One to view, and one nested to PATCH
 
-class ItemSerializer(serializers.ModelSerializer):
+
+class ItemViewSerializer(serializers.ModelSerializer):
     nodes = serializers.SerializerMethodField()
 
     class Meta:
         model = Item
         fields = [
-            'short_description',
-            'long_description',
-            'parent',
-            'nodes',
+            'id',
+            'project',
             'level',
             'item_type',
+            'parent',
+            'short_description',
+            'long_description',
+            'nodes',
         ]
 
     def get_nodes(self, obj):
-        return ItemSerializer(obj.get_children(), many=True).data
+        return ItemViewSerializer(obj.get_children(), many=True).data
+
+
+class ItemUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Item
+        fields = [
+            'id',
+            'level',
+            'parent',
+            'item_type',
+            'short_description',
+            'long_description',
+        ]
+
+
+class NestedItemSerializer(WritableNestedModelSerializer):
+    parent = ItemUpdateSerializer()
+
+    class Meta:
+        model = Item
+        fields = [
+            'id',
+            'level',
+            'parent',
+            'item_type',
+            'short_description',
+            'long_description',
+        ]
 
 
 class ProjectSerializer(serializers.ModelSerializer):
-    items = ItemSerializer(many=True, read_only=True)
+    items = ItemViewSerializer(many=True, read_only=True)
 
     class Meta:
         model = Project
