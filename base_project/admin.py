2aa685b21642e4a71da3071927d264cc14a9ad9b
faaiz <faaizajaz@gmail.com>
2021-07-12 17:00:51 +0500

    Removed old models and added current ones

--- base_project/admin.py
+++ base_project/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
-from .models import Project, Impact, Outcome, Output
+from .models import Project, Item
 
 admin.site.register(Project)
-admin.site.register(Impact)
-admin.site.register(Outcome)
-admin.site.register(Output)
+admin.site.register(Item)
 
 # Register your models here.
