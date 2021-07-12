25945d56b275525a804da5f26230ad582cb54573
faaiz <faaizajaz@gmail.com>
2021-07-12 19:39:02 +0500

    Various nulls and blanks

--- base_project/models.py
+++ base_project/models.py
@@ -1,20 +1,26 @@
 from django.db import models
 from mptt.models import MPTTModel, TreeForeignKey
 
-
+# TODO: Figure out all these nulls and blanks!
 class Project(models.Model):
     """
     The "Project" component of the project's logical framework. This is the
     highest level component. It defines a O2M relationship to "Impact"
     """
 
-    name = models.CharField(verbose_name="Project name", max_length=500)
-    short_description = models.CharField(
-        verbose_name="Short description of project", max_length=1000
+    name = models.CharField(
+        verbose_name="Project name", max_length=500, null=True, blank=True
     )
-    long_description = models.TextField(verbose_name="Long description of project")
+    short_description = models.CharField(
+        verbose_name="Short description of project",
+        max_length=1000,
+        null=True,
+        blank=True,
+    )
+    long_description = models.TextField(
+        verbose_name="Long description of project", null=True, blank=True
+    )
 
-    # TODO: null should not be allowed in production
     def __str__(self):
         return self.name
 
@@ -25,12 +31,18 @@ class Item(MPTTModel):
     lowest level component.
     """
 
-    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name="items")
-    item_type = models.CharField(verbose_name="Item type", max_length=500)
-    short_description = models.CharField(
-        verbose_name="Short description of Item", max_length=1000
+    project = models.ForeignKey(
+        Project, on_delete=models.CASCADE, related_name="items", null=True, blank=True
     )
-    long_description = models.TextField(verbose_name="Long description of Item")
+    item_type = models.CharField(
+        verbose_name="Item type", max_length=500, null=True, blank=True
+    )
+    short_description = models.CharField(
+        verbose_name="Short description of Item", max_length=1000, null=True, blank=True
+    )
+    long_description = models.TextField(
+        verbose_name="Long description of Item", null=True, blank=True
+    )
 
     parent = TreeForeignKey("self", blank=True, null=True, on_delete=models.DO_NOTHING)
 
