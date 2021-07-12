15cfb85b9a40b83a464713e71907b1e19d8504bf
faaiz <faaizajaz@gmail.com>
2021-07-12 19:38:39 +0500

    cleared up old routes and added a todo

--- base_project/urls.py
+++ base_project/urls.py
@@ -4,7 +4,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 
 urlpatterns = [
-    # TODO: Add delete views for all
+    # TODO: Add delete views for items
     # Project URLS
     path('allprojects/', views.ProjectList.as_view()),
     path('viewproject/<int:pk>/', views.ProjectDetail.as_view()),
@@ -12,18 +12,6 @@ urlpatterns = [
     path('allitems/', views.ItemList.as_view()),
     path('updateitem/<int:pk>/', views.ItemEdit.as_view()),
     path('createitem/', views.ItemCreate.as_view()),
-    # path('editproject/<int:pk>/', views.ProjectEdit.as_view()),
-    # # Impact URLs
-    # path('createimpact/', views.ImpactCreate.as_view()),
-    # path('editimpact/<int:pk>/', views.ImpactEdit.as_view()),
-    # path('deleteimpact/<int:pk>/', views.ImpactDelete.as_view()),
-    # # Outcome URLS
-    # path('createoutcome/', views.OutcomeCreate.as_view()),
-    # path('editoutcome/<int:pk>/', views.OutcomeEdit.as_view()),
-    # # Output URLS
-    # path('createoutput/', views.OutputCreate.as_view()),
-    # path('editoutput/<int:pk>/', views.OutputEdit.as_view()),
-    # path('deleteoutput/<int:pk>/', views.OutputDelete.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
