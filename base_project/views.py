13aca2a79242e2bc449e28c1f6c7fbe50b60ab9c
faaiz <faaizajaz@gmail.com>
2021-07-12 17:00:12 +0500

    Removed old views and added a new generic Item view

--- base_project/views.py
+++ base_project/views.py
@@ -1,15 +1,5 @@
-from .models import Outcome, Output, Project, Impact
-from serializers.flat_serializers import (
-    OutcomeSerializer,
-    OutputSerializer,
-    ProjectSerializer,
-    ImpactSerializer,
-)
-from serializers.nested_serializers import (
-    NestedImpactSerializer,
-    NestedOutcomeSerializer,
-    NestedProjectSerializer,
-)
+from .models import Project, Item
+from serializers.flat_serializers import ProjectSerializer, ItemSerializer
 from rest_framework import generics
 
 
@@ -44,93 +34,18 @@ class ProjectCreate(generics.CreateAPIView):
     serializer_class = ProjectSerializer
 
 
-class ProjectEdit(generics.UpdateAPIView):
-    """
-    Updates an existing Project object using the nested serializer
-    so can also edit relations to Impact and other objects. This is also
-    used to define initial relations, since we can't do so with
-    the ProjectCreate view as it uses a flat serializer.
-    """
-
-    queryset = Project.objects.all()
-    serializer_class = NestedProjectSerializer
+class ItemList(generics.ListAPIView):
+    queryset = Item.objects.filter(item_type="Impact")
+    serializer_class = ItemSerializer
 
 
-class ImpactCreate(generics.CreateAPIView):
-    """
-    Creates a new Impact object using the flat serializer. Only
-    used to create new impacts, not to define relation to Outcome.
-    """
+# class ProjectEdit(generics.UpdateAPIView):
+#     """
+#     Updates an existing Project object using the nested serializer
+#     so can also edit relations to Impact and other objects. This is also
+#     used to define initial relations, since we can't do so with
+#     the ProjectCreate view as it uses a flat serializer.
+#     """
 
-    queryset = Impact.objects.all()
-    serializer_class = ImpactSerializer
-
-
-class ImpactEdit(generics.UpdateAPIView):
-    """
-    Updates an existing Impact object using the nested serializer
-    so can also edit relations to Outcome objects. This is also
-    used to define initial relations, since we can't do so with
-    the ImpactCreate view as it uses a flat serializer.
-    """
-
-    queryset = Impact.objects.all()
-    serializer_class = NestedImpactSerializer
-
-
-class ImpactDelete(generics.DestroyAPIView):
-    """
-    Delete an existing Impact object using the flat serializer.
-    """
-
-    queryset = Impact.objects.all()
-    serializer_class = ImpactSerializer
-
-
-class OutcomeCreate(generics.CreateAPIView):
-    """
-    Creates a new Outcome object using the flat serializer. Only
-    used to create new Outcomes, not to define relation to Output.
-    """
-
-    queryset = Outcome.objects.all()
-    serializer_class = OutcomeSerializer
-
-
-class OutcomeEdit(generics.UpdateAPIView):
-    """
-    Updates an existing Outcome object using the nested serializer
-    so can also edit relations to Output objects. This is also
-    used to define initial relations, since we can't do so with
-    the OutcomeCreate view as it uses a flat serializer.
-    """
-
-    queryset = Outcome.objects.all()
-    serializer_class = NestedOutcomeSerializer
-
-
-class OutputCreate(generics.CreateAPIView):
-    """
-    Creates a new Output object using the flat serializer.
-    """
-
-    queryset = Output.objects.all()
-    serializer_class = OutputSerializer
-
-
-class OutputEdit(generics.UpdateAPIView):
-    """
-    Edit an existing Output object using the flat serializer.
-    """
-
-    queryset = Output.objects.all()
-    serializer_class = OutputSerializer
-
-
-class OutputDelete(generics.DestroyAPIView):
-    """
-    Delete an existing Output object using the flat serializer.
-    """
-
-    queryset = Output.objects.all()
-    serializer_class = OutputSerializer
+#     queryset = Project.objects.all()
+#     serializer_class = NestedProjectSerializer
