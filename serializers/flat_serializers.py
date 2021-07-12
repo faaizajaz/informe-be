e9f4cdc6d7368e0759797419c8557af08f43c2ce
faaiz <faaizajaz@gmail.com>
2021-07-12 17:00:38 +0500

    Removed unused serializers and added a recursive Item serializer

--- serializers/flat_serializers.py
+++ serializers/flat_serializers.py
@@ -1,27 +1,34 @@
 from rest_framework import serializers
-from base_project.models import Output, Project, Impact, Outcome
+from base_project.models import Item, Project
+
+
+class ItemSerializer(serializers.ModelSerializer):
+    nodes = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Item
+        fields = [
+            'short_description',
+            'long_description',
+            'parent',
+            'nodes',
+            'level',
+            'item_type',
+        ]
+
+    def get_nodes(self, obj):
+        return ItemSerializer(obj.get_children(), many=True).data
 
 
 class ProjectSerializer(serializers.ModelSerializer):
+    items = ItemSerializer(many=True, read_only=True)
+
     class Meta:
         model = Project
-        fields = ['id', 'name', 'long_description', 'short_description', 'impact']
-        depth = 6
+        fields = ['id', 'name', 'long_description', 'short_description', 'items']
 
 
-class ImpactSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Impact
-        fields = ['id', 'short_description', 'long_description']
-
-
-class OutcomeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Outcome
-        fields = ['id', 'short_description', 'long_description']
-
-
-class OutputSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Output
-        fields = ['id', 'short_description', 'long_description']
+# class ImpactSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Impact
+#         fields = ['id', 'short_description', 'long_description']
