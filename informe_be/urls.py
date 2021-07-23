955547da30c6632fe536890533c3749da591f17d
faaiz <faaizajaz@gmail.com>
2021-07-23 13:49:37 +0500

    Added DRF api-auth urls

--- informe_be/urls.py
+++ informe_be/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('base_project.urls')),
+    path('api-auth/', include('rest_framework.urls')),
 ]
