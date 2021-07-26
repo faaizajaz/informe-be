bf426a7081e8003e20c4daeea350377316e04f4b
faaiz <faaizajaz@gmail.com>
2021-07-26 15:23:25 +0500

    Removed unused drf urls

--- informe_be/urls.py
+++ informe_be/urls.py
@@ -19,5 +19,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('base_project.urls')),
-    path('api-auth/', include('rest_framework.urls')),
 ]
