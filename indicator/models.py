cfa3372328f6e259e0497343ab19326ff0a83b77
faaiz <faaizajaz@gmail.com>
2021-07-26 15:32:42 +0500

    Added Indicator and IndicatorEvidence models and ran migrations

--- indicator/models.py
+++ indicator/models.py
@@ -1,8 +1,23 @@
 from django.db import models
+from base_project.models import Item
 
 
 class Indicator(models.Model):
-    short_description = models.CharField(
-        verbose_name="Short description of indicator", max_length=1000
+    name = models.CharField(
+        verbose_name="Name of indicator", max_length=1000, null=True
     )
-    long_description = models.TextField(verbose_name="Long description of indicator")
+    description = models.TextField(verbose_name="Description of indicator", null=True)
+    item = models.ForeignKey(
+        Item,
+        verbose_name="Parent item",
+        related_name="indicator",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+    )
+
+
+class IndicatorEvidence(models.Model):
+    name = models.CharField(verbose_name="Name of evidence", max_length=1000, null=True)
+    description = models.TextField(verbose_name="Description of evidence", null=True)
+    indicator = models.ManyToManyField(Indicator, related_name="indicator", null=True)
