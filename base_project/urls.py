2dcc5ced03c14753d893f9b1dc5625d7f5acfd6e
faaiz <faaizajaz@gmail.com>
2021-07-26 15:23:00 +0500

    Added a todo

--- base_project/urls.py
+++ base_project/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from . import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
-
+# TODO: CHange to /project/all /item/create etc.
 urlpatterns = [
     path('user-auth/', include('account.urls')),
     path('allprojects/', views.ProjectList.as_view()),
