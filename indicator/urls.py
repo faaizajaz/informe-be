332547a068d89cac4f39dd446942963ff5a242ec
faaiz <faaizajaz@gmail.com>
2021-07-26 16:39:32 +0500

    Added indicator urls

---
+++ indicator/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from . import views
+from rest_framework.urlpatterns import format_suffix_patterns
+
+# TODO: CHange to /project/all /item/create etc.
+urlpatterns = [
+    path('view/<int:pk>/', views.IndicatorDetail.as_view()),
+    path('create/', views.IndicatorCreate.as_view()),
+]
+
+# urlpatterns = format_suffix_patterns(urlpatterns)
