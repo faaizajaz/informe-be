47984952c95aaef796f1dd324f807ac8df17f97e
faaiz <faaizajaz@gmail.com>
2021-07-26 16:39:26 +0500

    Created indicator detail and create views

--- indicator/views.py
+++ indicator/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Indicator, IndicatorEvidence
+from rest_framework import generics, permissions
+from serializers.serializers import IndicatorSerializer
 
-# Create your views here.
+
+class IndicatorDetail(generics.RetrieveAPIView):
+    queryset = Indicator.objects.all()
+    serializer_class = IndicatorSerializer
+
+
+class IndicatorCreate(generics.CreateAPIView):
+    queryset = Indicator.objects.all()
+    serializer_class = IndicatorSerializer
