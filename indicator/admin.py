793dcaafc8ac4b6e245c14a193109f2d6f7a4da4
faaiz <faaizajaz@gmail.com>
2021-07-26 16:05:23 +0500

    Added models to admin

--- indicator/admin.py
+++ indicator/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Indicator, IndicatorEvidence
+
+admin.site.register(Indicator)
+admin.site.register(IndicatorEvidence)
+
 
 # Register your models here.
