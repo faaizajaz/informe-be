b4bcaf73f775264666c8446e5fe09a16a2ad2a00
faaiz <faaizajaz@gmail.com>
2021-07-27 14:42:12 +0500

    settings for development

--- informe_be/urls.py
+++ informe_be/urls.py
@@ -17,6 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+# TODO: Remove static settings before deploy
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/user-auth/', include('account.urls')),
@@ -24,6 +28,6 @@ urlpatterns = [
     path('api/item/', include('base_project.urls.item_urls')),
     path('api/indicator/', include('indicator.urls.indicator_urls')),
     path('api/evidence/', include('indicator.urls.evidence_urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns = format_suffix_patterns(urlpatterns)
