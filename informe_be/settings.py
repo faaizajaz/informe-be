ff7566ee5975e406200df269c83eb425b7223845
faaiz <faaizajaz@gmail.com>
2021-07-27 14:42:02 +0500

    Configured media settings for uploaded files

--- informe_be/settings.py
+++ informe_be/settings.py
@@ -142,3 +142,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# MEDIA
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
