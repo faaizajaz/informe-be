779061c8695f20f84ddcf1549f6628942362210a
faaiz <faaizajaz@gmail.com>
2021-07-27 12:53:13 +0500

    Removed old appconfig

--- informe_be/settings.py
+++ informe_be/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'activity.apps.ActivityConfig',
     'indicator.apps.IndicatorConfig',
     'base_project.apps.BaseProjectConfig',
     'account.apps.AccountConfig',
