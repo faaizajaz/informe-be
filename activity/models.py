0aa8a888ce4af9278497bc32d9b9b797f59b032d
faaiz <faaizajaz@gmail.com>
2021-07-27 11:54:07 +0500

    Deleted activity app--I'll come back to this later

--- activity/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-class Activity(models.Model):
-    short_description = models.CharField(
-        verbose_name="Short description of activity", max_length=1000
-    )
-    long_description = models.TextField(verbose_name="Long description of activity")
