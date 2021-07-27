dff1892497ffd3014747fda59e32a2bade4e5868
faaiz <faaizajaz@gmail.com>
2021-07-27 12:55:22 +0500

    Started setting up views. Still need to figure out file upload view

--- indicator/views.py
+++ indicator/views.py
@@ -1,14 +1,35 @@
 from django.shortcuts import render
+from rest_framework.response import Response
 from .models import Indicator, IndicatorEvidence
 from rest_framework import generics, permissions
-from serializers.serializers import IndicatorSerializer
+from rest_framework.parsers import FileUploadParser
+from serializers.serializers import (
+    IndicatorEvidenceSerializer,
+    IndicatorViewSerializer,
+    IndicatorCreateSerializer,
+)
 
 
 class IndicatorDetail(generics.RetrieveAPIView):
     queryset = Indicator.objects.all()
-    serializer_class = IndicatorSerializer
+    serializer_class = IndicatorViewSerializer
 
 
 class IndicatorCreate(generics.CreateAPIView):
     queryset = Indicator.objects.all()
-    serializer_class = IndicatorSerializer
+    serializer_class = IndicatorCreateSerializer
+
+
+class IndicatorEvidenceDetail(generics.RetrieveAPIView):
+    queryset = IndicatorEvidence.objects.all()
+    serializer_class = IndicatorEvidenceSerializer
+
+
+class IndicatorEvidenceCreate(generics.CreateAPIView):
+    queryset = IndicatorEvidence.objects.all()
+    # serializer_class = IndicatorEvidenceSerializer
+    parser_classes = [FileUploadParser]
+
+    # def put(self, request, filename, format=None):
+    #     print(request.data)
+    #     return Response(status=204)
