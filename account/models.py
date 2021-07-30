b7ae1715e540931aa236535741f409e9860d8a48
faaiz <faaizajaz@gmail.com>
2021-07-30 13:50:03 +0500

    Added current_org field to CustomUser and ran migrations

--- account/models.py
+++ account/models.py
@@ -1,13 +1,27 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class CustomUser(AbstractUser):
-    pass
-    # Additional fields here
+    current_org = models.IntegerField(
+        verbose_name="Last organization active", default=None
+    )
+    # ##### RELATED FIELDS ##### #
+    # project_reported
+    #   TO: Project
+    #   TYPE: M2M
+    #
+    # project_owned
+    #   TO: Project
+    #   TYPE: M2M
+    #
+    # org_joined
+    #   TO: Organization
+    #   TYPE: M2M
+    #
+    # org_owned
+    #   TO: Organization
+    #   TYPE: M2M
 
     def __str__(self):
         return self.username
-
-
-# Create your models here.
