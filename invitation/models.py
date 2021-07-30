c86e678f13305c3c5322f4e15c0a0dad4fb9c43d
faaiz <faaizajaz@gmail.com>
2021-07-30 17:05:51 +0500

    Created OrgINvitation model and migrated

--- invitation/models.py
+++ invitation/models.py
@@ -1,3 +1,12 @@
+import uuid
+
+from account.models import CustomUser
+from base_project.models import Organization
 from django.db import models
 
-# Create your models here.
+
+class OrgInvitation(models.Model):
+    sender = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    receiver_email = models.EmailField()
+    organization = models.OneToOneField(Organization, on_delete=models.CASCADE)
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False)
