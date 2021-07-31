7206fb8be1492f2ca020ed3a738848e161d4b778
faaiz <faaizajaz@gmail.com>
2021-07-31 16:36:22 +0500

    Fixed relationship. Should not be a onetoone field.

--- invitation/models.py
+++ invitation/models.py
@@ -8,5 +8,5 @@ from django.db import models
 class OrgInvitation(models.Model):
     sender = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
     receiver_email = models.EmailField()
-    organization = models.OneToOneField(Organization, on_delete=models.CASCADE)
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
     uid = models.UUIDField(default=uuid.uuid4, editable=False)
