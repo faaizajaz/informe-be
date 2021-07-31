08e65cd04434f1f9555af160517b2ee3a1a4b074
faaiz <faaizajaz@gmail.com>
2021-07-31 15:53:33 +0500

    Changed name of uuid field to avoid conflict with uuid library

--- invitation/models.py
+++ invitation/models.py
@@ -9,4 +9,4 @@ class OrgInvitation(models.Model):
     sender = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
     receiver_email = models.EmailField()
     organization = models.OneToOneField(Organization, on_delete=models.CASCADE)
-    uuid = models.UUIDField(default=uuid.uuid4, editable=False)
+    uid = models.UUIDField(default=uuid.uuid4, editable=False)
