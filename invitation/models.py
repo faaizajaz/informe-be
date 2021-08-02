6ad034b923d4bb7ddee69232803979dd556b5655
faaiz <faaizajaz@gmail.com>
2021-08-02 12:42:00 +0500

    Made foreignkey and ran migration

--- invitation/models.py
+++ invitation/models.py
@@ -6,7 +6,7 @@ from django.db import models
 
 
 class OrgInvitation(models.Model):
-    sender = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    sender = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     receiver_email = models.EmailField()
     organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
     uid = models.UUIDField(default=uuid.uuid4, editable=False)
