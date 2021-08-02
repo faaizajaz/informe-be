ddabfc648717261fbb80ad4e71ee4ea1a221aa9e
faaiz <faaizajaz@gmail.com>
2021-08-02 15:38:57 +0500

    Added notification signals. New signals will be added for any notification that needs to be sent

---
+++ notification/signals.py
@@ -0,0 +1,13 @@
+import django.dispatch
+
+org_invitation_received = django.dispatch.Signal(
+    providing_args=["notification_receiver", "org", "invitation_sender"]
+)
+
+org_invitation_accepted = django.dispatch.Signal(
+    providing_args=["notification_receiver", "org"]
+)
+
+
+# SEND BY:
+# org_invitation_received.send(sender=self, instance=self.request.user)
