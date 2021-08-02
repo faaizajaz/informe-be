9f0e107e15c68e5ebdda0d51cad328fe465536f6
faaiz <faaizajaz@gmail.com>
2021-08-02 15:25:54 +0500

    Create Notification model and ran migrations

--- notification/models.py
+++ notification/models.py
@@ -1,3 +1,20 @@
+from account.models import CustomUser
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Notification(models.Model):
+    receiver = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE, related_name='notifications'
+    )
+    subject = models.CharField(max_length=250)
+    message = models.CharField(max_length=2500)
+    created = models.DateTimeField(default=timezone.now, editable=False)
+    unread = models.BooleanField(default=True)
+
+    # "sender" - generic relation to any model.
+    # content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    # object_id = models.PositiveIntegerField()
+    # content_object = GenericForeignKey()
