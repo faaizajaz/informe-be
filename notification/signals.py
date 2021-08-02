198b76444f0fbc0808cbc324d9dbbf6cd10ba2af
faaiz <faaizajaz@gmail.com>
2021-08-02 16:31:26 +0500

    Removed comment

--- notification/signals.py
+++ notification/signals.py
@@ -7,7 +7,3 @@ org_invitation_received = django.dispatch.Signal(
 org_invitation_accepted = django.dispatch.Signal(
     providing_args=["notification_receiver", "org", "invitation_receiver"]
 )
-
-
-# SEND BY:
-# org_invitation_received.send(sender=self, instance=self.request.user)
