29e7b504a24a84c0f44e6c372e5827441ec64de6
faaiz <faaizajaz@gmail.com>
2021-08-09 15:34:02 +0500

    Added a user register serializer with overridden create method to check for passowrd match, and create inactive user

--- account/serializers.py
+++ account/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.response import Response
 
 from .models import CustomUser
 
@@ -7,3 +8,27 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ['id', 'email', 'username', 'first_name', 'last_name']
+
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password2 = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'email', 'password', 'password2']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        password2 = validated_data.pop('password2')
+
+        if password == password2:
+            user = CustomUser(**validated_data)
+            user.set_password(password)
+            # New users should not be active
+            user.is_active = False
+            # TODO: Send registration confirmation
+            user.save()
+            return user
+        else:
+            raise serializers.ValidationError({"detail": "Passwords do not match."})
