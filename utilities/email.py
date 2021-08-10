264dc14649d49846b44c9e26f85f2cab87cfdf79
faaiz <faaizajaz@gmail.com>
2021-08-10 13:56:34 +0500

    Added a useless docstring

--- utilities/email.py
+++ utilities/email.py
@@ -3,6 +3,9 @@ from informe_be import settings
 
 
 def send_email(to, subject, body):
+    """
+    Sends an email
+    """
     from_email = settings.EMAIL_HOST_USER
 
     email = EmailMessage(subject=subject, body=body, from_email=from_email, to=to)
