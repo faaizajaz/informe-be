6009f52d50628048990a1863577629b69c9581f3
faaiz <faaizajaz@gmail.com>
2021-08-16 15:57:15 +0500

    Removed the unused route

--- base_project/urls/org_urls.py
+++ base_project/urls/org_urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from .. import views
 
 urlpatterns = [
-    path('all/', views.OrgList.as_view()),
     path('allorgprojects/', views.OrgAllProjects.as_view()),
     path('create/', views.OrgCreate.as_view()),
     path('updateowner/<int:pk>/', views.OrgOwnerEdit.as_view()),
